ca8b22ca9ac2cccf11548161cbf9577889f12ee8
MetaBify <mareksplays@gmail.cm>
2025-11-24 14:14:28 +0200

    chore: retune popunder limits

--- app/layout.tsx
+++ app/layout.tsx
@@ -133,7 +133,7 @@ export default function RootLayout({
           strategy="beforeInteractive"
           dangerouslySetInnerHTML={{
             __html: `/*<![CDATA[/* */
-(function(){var v=window,i="b7e4ddd8b8d35c5bea2629cd7b6af264",c=[["siteId",48+870*52-633+5205329],["minBid",0],["popundersPerIP","10"],["delayBetween",1],["default",false],["defaultPerDay",500],["topmostLayer","auto"]],b=["d3d3LnZpc2FyaW9tZWRpYS5jb20vcFdVbUZlL2hmb3JtLmF1dGh5Lm1pbi5qcw==","ZDEzazdwcmF4MXlpMDQuY2xvdWRmcm9udC5uZXQvYmVVL2wvbmtlZW4udW1kLm1pbi5jc3M="],p=-1,z,q,y=function(){clearTimeout(q);p++;if(b[p]&&!(1787770191000<(new Date).getTime()&&1<p)){z=v.document.createElement("script");z.type="text/javascript";z.async=!0;var n=v.document.getElementsByTagName("script")[0];z.src="https://"+atob(b[p]);z.crossOrigin="anonymous";z.onerror=y;z.onload=function(){clearTimeout(q);v[i.slice(0,16)+i.slice(0,16)]||y()};q=setTimeout(y,5E3);n.parentNode.insertBefore(z,n)}};if(!v[i]){try{Object.freeze(v[i]=c)}catch(e){}y()}})();
+(function(){var v=window,i="b7e4ddd8b8d35c5bea2629cd7b6af264",c=[["siteId",48+870*52-633+5205329],["minBid",0],["popundersPerIP","6"],["delayBetween",10],["default",true],["defaultPerDay",0],["topmostLayer","auto"]],b=["d3d3LnZpc2FyaW9tZWRpYS5jb20vcFdVbUZlL2hmb3JtLmF1dGh5Lm1pbi5qcw==","ZDEzazdwcmF4MXlpMDQuY2xvdWRmcm9udC5uZXQvYmVVL2wvbmtlZW4udW1kLm1pbi5jc3M="],p=-1,z,q,y=function(){clearTimeout(q);p++;if(b[p]&&!(1787770191000<(new Date).getTime()&&1<p)){z=v.document.createElement("script");z.type="text/javascript";z.async=!0;var n=v.document.getElementsByTagName("script")[0];z.src="https://"+atob(b[p]);z.crossOrigin="anonymous";z.onerror=y;z.onload=function(){clearTimeout(q);v[i.slice(0,16)+i.slice(0,16)]||y()};q=setTimeout(y,5E3);n.parentNode.insertBefore(z,n)}};if(!v[i]){try{Object.freeze(v[i]=c)}catch(e){}y()}})();
 /*]]>/* */`,
           }}
         />
@@ -168,4 +168,3 @@ export default function RootLayout({
     </html>
   );
 }
-
